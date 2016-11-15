ae8d61268e494530b20a2a2cb057dc14afffddbb
kevin <kevin@qudos.com>
2016-11-15 11:54:23 -0800

    Change debug statements to info

--- tcelery/redis.py
+++ tcelery/redis.py
@@ -65,7 +65,7 @@ class RedisConsumer(object):
                 timedelta(milliseconds=expires), self.on_timeout, key)
         else:
             timeout = None
-        logger.debug("Subscribing to key %s (%s) with timeout %s",
+        logger.info("Subscribing to key %s (%s) with timeout %s",
                      task_id, key, timeout)
         self.subscriber.subscribe(
             key, partial(self.on_result, key, callback, timeout))
@@ -73,15 +73,15 @@ class RedisConsumer(object):
     def on_result(self, key, callback, timeout, result):
         if timeout:
             self.producer.conn_pool.io_loop.remove_timeout(timeout)
-        logger.debug("Got result for key %s, unsubscribing...", key)
+        logger.info("Got result for key %s, unsubscribing...", key)
 
         self.subscriber.unsubscribe_channel(
             key, partial(self.on_unsubscribed, key, callback, result))
 
     def on_unsubscribed(self, key, callback, result):
-        logger.debug("Unsubscribed from %s", key)
+        logger.info("Unsubscribed from %s", key)
         callback(result)
 
     def on_timeout(self, key):
-        logger.debug("Task timed out on key %s")
+        logger.info("Task timed out on key %s")
         self.subscriber.unsubscribe_channel(key)
