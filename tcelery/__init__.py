9c39a35fade2c5eb4127907591c969f6dc82c94a
Qubot <admin@qudos.com>
2016-11-22 15:39:50 -0800

    Bump version: 99.3.10 → 99.3.11

--- tcelery/__init__.py
+++ tcelery/__init__.py
@@ -8,7 +8,7 @@ from .connection import ConnectionPool
 from .producer import NonBlockingTaskProducer
 from .result import AsyncResult
 
-__version__ = '99.3.10'
+__version__ = '99.3.11'
 VERSION = tuple(int(s) for s in __version__.split('.'))
 
 
